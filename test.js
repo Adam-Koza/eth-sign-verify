9c03563b1e0b3e314af9838d46bdb1cbd3d4b4b7
Adam-Koza <adam.koza@mail.utoronto.ca>
2019-04-11 20:08:32 -0400

    Remove package dependency from test file.

--- test.js
+++ test.js
@@ -1,7 +1,6 @@
 let signveri = require('./index.js');
-let ethers = require('ethers');
 
 signveri.signMessage("Test Message", "0x0123456789012345678901234567890123456789012345678901234567890123").then(sig => {
     console.log(sig);
-    let wallet = new ethers.Wallet("0x0123456789012345678901234567890123456789012345678901234567890123");
-    return signveri.verifyMessage("Test Message", sig, wallet.address)}).then(console.log);
+    return signveri.verifyMessage("Test Message", sig, "0x14791697260E4c9A71f18484C9f997B308e59325")
+}).then(console.log);
